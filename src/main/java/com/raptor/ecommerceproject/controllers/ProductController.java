f9c9ff45b0b5d457b3fc7f99ae385feae60bfbe1
Fabian Chuqui <81194167+FabianCh13395@users.noreply.github.com>
2023-03-22 08:26:19 -0500

    templates de productos , casi como la funcionalidad de guardar un producto

--- src/main/java/com/raptor/ecommerceproject/controllers/ProductController.java
+++ src/main/java/com/raptor/ecommerceproject/controllers/ProductController.java
@@ -1,16 +1,43 @@
 package com.raptor.ecommerceproject.controllers;
 
+import com.raptor.ecommerceproject.models.Product;
+import com.raptor.ecommerceproject.models.User;
+import com.raptor.ecommerceproject.services.ProductService;
+import org.slf4j.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+
 
 @Controller
 @RequestMapping("/products")
 public class ProductController {
+    private final Logger LOGGER= LoggerFactory.getLogger(ProductController.class);
 
+    @Autowired
+    private ProductService productService;
 
     @GetMapping("")
     public String homeProduct(){
         return "products/show";
     }
+
+    @GetMapping("/create")
+    public String viewCreate(){
+        return "products/create";
+    }
+
+    @PostMapping("/save")
+    public String saveProduct(Product product){
+        LOGGER.info("Este es el objeto producto {}",product);
+        User user=new User(1L,"","","","","","","");
+        product.setUser(user);
+        productService.save(product);
+        return "redirect:/products";
+    }
+
+
 }
