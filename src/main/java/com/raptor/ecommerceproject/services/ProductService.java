af092982e17e4687bfdfa73e8f38f58be4e2e4c5
Fabian Chuqui <81194167+FabianCh13395@users.noreply.github.com>
2023-03-23 06:43:27 -0500

    Implementacion del metodo para listar todos los productos para el administrador

--- src/main/java/com/raptor/ecommerceproject/services/ProductService.java
+++ src/main/java/com/raptor/ecommerceproject/services/ProductService.java
@@ -2,15 +2,18 @@ package com.raptor.ecommerceproject.services;
 
 import com.raptor.ecommerceproject.models.Product;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ProductService {
-    //Metodo para implementar y guardar
+    //Método para implementar y guardar
     public Product save(Product product);
-    //metodo para implementar y buscar un producto si es que existe o no en la base de datos.
+    //Método para implementar y buscar un producto si es que existe o no en la base de datos.
     public Optional<Product> get(Long id);
-    //Metodo para implementar y actualizar un producto
+    //Método para implementar y actualizar un producto
     public void update(Product product);
-    //Metodo a implementar y eliminar un producto
+    //Método a implementar y eliminar un producto
     public void delete(Long id);
+    //Método para listar todo
+    public List<Product> findAll();
 }
