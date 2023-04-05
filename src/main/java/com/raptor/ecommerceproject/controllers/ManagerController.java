db2581e1bba72a31baf95a6f8741ab878398c4ab
Fabian Chuqui <81194167+FabianCh13395@users.noreply.github.com>
2023-04-05 06:39:11 -0500

    Implementación de los detalles de cada orden tanto en la vista como en el controlador

--- src/main/java/com/raptor/ecommerceproject/controllers/ManagerController.java
+++ src/main/java/com/raptor/ecommerceproject/controllers/ManagerController.java
@@ -1,13 +1,17 @@
 package com.raptor.ecommerceproject.controllers;
 
+import com.raptor.ecommerceproject.models.Order;
 import com.raptor.ecommerceproject.models.Product;
 import com.raptor.ecommerceproject.services.IOrderService;
 import com.raptor.ecommerceproject.services.ProductService;
 import com.raptor.ecommerceproject.services.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -25,6 +29,9 @@ public class ManagerController {
     @Autowired
     private IOrderService iOrderService;
 
+    private Logger logger= LoggerFactory.getLogger(ManagerController.class);
+
+
     @GetMapping("")
     public String home(Model model){
         List<Product> products=productService.findAll();
@@ -38,9 +45,19 @@ public class ManagerController {
         return "manager/usuarios";
     }
 
+    //Ver todas las ordenes
     @GetMapping("/orders")
     public String ordersAll(Model model){
         model.addAttribute("orders",iOrderService.findAll());
         return "manager/ordenes";
+    }
+    //Ver detalle de cada orden
+    @GetMapping("/detail/{id}")
+    public String detailOrder(@PathVariable Long id,Model model){
+        logger.info("Este es el id de la orden",id);
+        Order order=iOrderService.findById(id).get();
+        model.addAttribute("details",order.getDetails());
+
+        return "manager/detalleorden";
     }
 }
